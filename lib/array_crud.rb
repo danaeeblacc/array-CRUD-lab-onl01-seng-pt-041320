c2e726ed7a4ead0cba7a9e63fe2eec3cfc8d8f95
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-19 00:07:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,13 +9,13 @@ end
 def add_element_to_end_of_array(array, element)
   "array!"
 end
-["I", "am","really", "learning","array!"]
+
 def add_element_to_start_of_array(array, element)
- 
+ ["I", "am","really", "learning","array!"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+ ["wow", "I", "am", "really",] 
 end
 
 def remove_element_from_start_of_array(array)
